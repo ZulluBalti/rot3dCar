8bdec0a09dce90c764fa4e140451ef763b40d38c
Zullu Balti <zullubalti@gmail.com>
2021-05-10 07:56:15 +0500

    fix minor bugs to make it runable

--- render.js
+++ render.js
@@ -1,71 +1,72 @@
-import {OBJLoader} from './Plugins/OBJLoader.js';
+import { OBJLoader } from "./build/OBJLoader.js";
+import * as THREE from "./build/three.module.js";
 
 var renderer, scene, camera, banana;
 
 var ww = window.innerWidth,
-	wh = window.innerHeight;
+  wh = window.innerHeight;
 
-function init(){
+function init() {
+  renderer = new THREE.WebGLRenderer();
+  document.querySelector(".scene").appendChild(renderer.domElement);
+  renderer.setSize(ww, wh);
 
-	renderer = new THREE.WebGLRenderer({canvas : document.getElementById('scene')});
-	renderer.setSize(ww,wh);
+  scene = new THREE.Scene();
 
-	scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(50, ww / wh, 0.1, 10000);
+  camera.position.set(0, 0, 500);
+  scene.add(camera);
 
-	camera = new THREE.PerspectiveCamera(50,ww/wh, 0.1, 10000 );
-	camera.position.set(0,0,500);
-	scene.add(camera);
+  //Add a light in the scene
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
+  directionalLight.position.set(0, 0, 350);
+  directionalLight.lookAt(new THREE.Vector3(0, 0, 0));
+  scene.add(directionalLight);
 
-	//Add a light in the scene
-	directionalLight = new THREE.DirectionalLight( 0xffffff, 0.8 );
-	directionalLight.position.set( 0, 0, 350 );
-	directionalLight.lookAt(new THREE.Vector3(0,0,0));
-	scene.add( directionalLight );
-
-	//Load the obj file
-	loadOBJ();
+  //Load the obj file
+  loadOBJ();
 }
 
-var loadOBJ = function(){
+var loadOBJ = function () {
+  //Manager from ThreeJs to track a loader and its status
+  var manager = new THREE.LoadingManager();
+  //Loader for Obj from Three.js
+  var loader = new OBJLoader(manager);
 
-	//Manager from ThreeJs to track a loader and its status
-	var manager = new THREE.LoadingManager();
-	//Loader for Obj from Three.js
-	var loader = new THREE.OBJLoader( manager );
-
-	//Launch loading of the obj file, addBananaInScene is the callback when it's ready
-	loader.load( 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/127738/banana.obj', addBananaInScene);
-
+  //Launch loading of the obj file, addBananaInScene is the callback when it's ready
+  loader.load(
+    "https://s3-us-west-2.amazonaws.com/s.cdpn.io/127738/banana.obj",
+    addBananaInScene
+  );
 };
 
-var addBananaInScene = function(object){
-	banana = object;
-	//Move the banana in the scene
-	banana.rotation.x = Math.PI/2;
-	banana.position.y = -200;
-	banana.position.z = 50;
-	//Go through all children of the loaded object and search for a Mesh
-	object.traverse( function ( child ) {
-		//This allow us to check if the children is an instance of the Mesh constructor
-		if(child instanceof THREE.Mesh){
-			child.material.color = new THREE.Color(0X00FF00);
-			//Sometimes there are some vertex normals missing in the .obj files, ThreeJs will compute them
-			child.geometry.computeVertexNormals();
-		}
-	});
-	//Add the 3D object in the scene
-	scene.add(banana);
-	render();
+var addBananaInScene = function (object) {
+  banana = object;
+  //Move the banana in the scene
+  banana.rotation.x = Math.PI / 2;
+  banana.position.y = -200;
+  banana.position.z = 50;
+  //Go through all children of the loaded object and search for a Mesh
+  object.traverse(function (child) {
+    //This allow us to check if the children is an instance of the Mesh constructor
+    if (child instanceof THREE.Mesh) {
+      child.material.color = new THREE.Color(0x00ff00);
+      //Sometimes there are some vertex normals missing in the .obj files, ThreeJs will compute them
+      child.geometry.computeVertexNormals();
+    }
+  });
+  //Add the 3D object in the scene
+  scene.add(banana);
+  render();
 };
-
 
 var render = function () {
-	requestAnimationFrame(render);
+  requestAnimationFrame(render);
 
-	//Turn the banana
-	banana.rotation.z += .01;
+  //Turn the banana
+  banana.rotation.z += 0.01;
 
-	renderer.render(scene, camera);
+  renderer.render(scene, camera);
 };
 
 init();
